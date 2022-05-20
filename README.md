3bd995b1b7f761f4b4c7b9e60f80ed02a2f66638
Maiara Martins <maiaramartins093@gmail.com>
2022-05-19 21:28:22 -0300

    doc: readme

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Conjugate Bead
 
-Package to return a collection of Conjugates of integers from another collection of integers (Transposed Matrix)
+Package to return a collection of Conjugates of integers from another collection of integers (Transposed Matrix). was use in this project **ESLint**, **Typescript** and **Jest** were used for the unit tests
+
+```text
+ npm install
+ ```
 
 ## Start Application
 
---
+++ lib/index.d.ts
@@ -0,0 +1,10 @@
+export declare const EmptyError: string;
+export declare const NegativeError: string;
+export declare const WrongFormat: string;
+/**
+ * @param {number[]}  numbers - A colletions of integers.
+ * @returns {number[]} collection of conjugates of integers
+ */
+export declare function getConjugateCollection(numbers: number[]): number[];
+export declare function printMatrix(numbers: number[]): void;
+export declare function handlerStringOfNumbers(answer: string): number[];
